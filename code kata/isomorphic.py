dfc2b5f84781912c2635d7a124bd37c811cc4830
Abiyash <51644520+Abiyash@users.noreply.github.com>
2019-06-23 08:22:22 -0700

    Update isomorphic.py

--- code kata/isomorphic.py
+++ code kata/isomorphic.py
@@ -1,9 +1,9 @@
-p,q = raw_input().split()
-count = 0
+p, q = input().split()
+co = 0
 for i in range(len(p)):
   if p.count(p[i]) == q.count(q[i]):
-    count += 1
-if count == len(p):
-  print "yes"
+    co += 1
+if co == len(p):
+  print ("yes")
 else:
-  print "no"
+  print ("no")
