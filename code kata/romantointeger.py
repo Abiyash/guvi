b9a41fc7133a79616cd13c12d55c16e616608074
Abiyash <51644520+Abiyash@users.noreply.github.com>
2019-06-23 08:15:42 -0700

    Create romantointeger.py

---
+++ code kata/romantointeger.py
@@ -0,0 +1,5 @@
+pqw=input()
+xyz=["0","I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX"]
+for i in range(21):
+  if(pqw==xyz[i]):
+    print(i)
